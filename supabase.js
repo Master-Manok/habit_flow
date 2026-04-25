81caa7c4b8d1f6b7153b6a2346f93a914c8c6459
Master-Manok <manojkumars_11706@outlook.com>
2026-04-25 11:47:04 +0530

    supabase.ju updated the url and anon key

--- supabase.js
+++ supabase.js
@@ -5,8 +5,8 @@
 //  from: Supabase Dashboard → Settings → API
 // ─────────────────────────────────────────────────────────────
 
-const SUPABASE_URL      = 'https://YOUR_PROJECT.supabase.co';
-const SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
+const SUPABASE_URL      = 'https://mikrmkicllqwsnzqtgwf.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pa3Jta2ljbGxxd3NuenF0Z3dmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwOTIzMDEsImV4cCI6MjA5MjY2ODMwMX0.2xjCvhiEE5uomIU_acdo6iRzklPRjdCXKxPWlly-45Q';
 
 const { createClient } = supabase;
 const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
